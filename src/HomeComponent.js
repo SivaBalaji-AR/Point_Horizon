6d36a3333d3a30c3804ba69c33f0b88457603d91
SivaBalaji-AR <151618924+SivaBalaji-AR@users.noreply.github.com>
2024-01-16 08:12:21 +0530

    Update HomeComponent.js

--- src/HomeComponent.js
+++ src/HomeComponent.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { UserContext } from './UserContext';
 import { useNavigate } from 'react-router-dom';
+import './HomeComponent.css';
 
 const HomeComponent = () => {
   const { user, setUser } = useContext(UserContext);
@@ -12,15 +13,32 @@ const logout = () => {
     navigate('/');
 };
 
-  if (!user) return <div>
-        Please log in
-        <button onClick={()=>navigate('/')}>Go to login page</button></div>;
+  if (!user) 
+  {
+  return <div className='outerdiv'>
+  <div className='innerdiv'>
+        <p>Please log in!</p>
+        <div>
+        <button onClick={()=>navigate('/')} className='button-65'>Go to login</button>
+        </div>
+        </div>;
+        </div>
+  
+  }
+
+  let imgurl = 'https://i.pinimg.com/736x/5f/f7/76/5ff7765c603157c920313155699fd001.jpg'
 
   return (
-    <div>
-      Welcome, {user.username}!
-      <button onClick={()=>{navigate('/home')}}>Book Tickets</button>
-      <button onClick={logout}>Logout</button>
+    <div className='outerdiv'>
+      <div class='innerdiv'>
+      <div><img src={imgurl} alt='fuck' className='imgsrc1'/></div>
+        <p>Welcome User: {user.username}</p>
+        <div >
+        <button onClick={()=>{navigate('/home')}} className='button-65'>Book Tickets</button>
+        <button onClick={logout} className='button-65'>Logout</button>
+        </div>
+       
+      </div>
     </div>
   );
 };
