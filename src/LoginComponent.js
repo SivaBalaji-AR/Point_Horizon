7446f91a91a4da5239387e1aeaef359e25351e74
SivaBalaji-AR <151618924+SivaBalaji-AR@users.noreply.github.com>
2024-01-16 08:18:36 +0530

    Update LoginComponent.js

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from './UserContext'; // Import UserContext
+import './LoginComponent.css';
 
 const LoginComponent = () => {
   const [users, setUsers] = useState([]);
@@ -10,7 +11,7 @@ const LoginComponent = () => {
   const { setUser } = useContext(UserContext); // Use the context
 
   useEffect(() => {
-    axios.get('https://6566de2d64fcff8d730f2d13.mockapi.io/users')
+    axios.get('https://6564372fceac41c0761da0ae.mockapi.io/api/v1/posts')
       .then(response => setUsers(response.data))
       .catch(error => console.error('Error:', error));
   }, []);
@@ -18,12 +19,15 @@ const LoginComponent = () => {
   const handleLogin = (e) => {
     e.preventDefault();
     const foundUsers = users.filter(user => user.username === loginDetails.username);
-    
-    if (foundUsers.length > 0) {
+    if (foundUsers.length>0 && foundUsers[0].password===loginDetails.password) {
       setUser(foundUsers[0]); // Set the found user in context
       alert('Login Successful');
       navigate('/hc')
-    } else {
+    } 
+    else if(foundUsers.length>0){
+      alert('incorrect Login Details');
+    }
+    else {
       alert('User not found. Please Sign Up');
       navigate('/signup')
     }
@@ -33,14 +37,21 @@ const LoginComponent = () => {
     setLoginDetails({ ...loginDetails, [e.target.name]: e.target.value });
   };
 
+  let imgurl = 'https://t3.ftcdn.net/jpg/03/52/90/48/360_F_352904882_Uv8ZuVZNduEuE9KyjscqF1sXc2YzDppY.jpg'
+
   return (
-    <form onSubmit={handleLogin}>
+    <div className='outerdiv'>
+    <div className='innerdiv'>
+    <div><img src={imgurl} alt='fuck' className='imgsrc'/></div>
+    <form onSubmit={handleLogin} className='outerform'>
+      
       <input
         name="username"
         type="text"
         placeholder="Username"
         value={loginDetails.username}
         onChange={handleInputChange}
+        className='input'
       />
       <input
         name="password"
@@ -48,9 +59,16 @@ const LoginComponent = () => {
         placeholder="Password"
         value={loginDetails.password}
         onChange={handleInputChange}
+        className='input'
       />
-      <button type="submit">Login</button>
+      <button type="submit" class="button-63">Login</button>
     </form>
+    <div className='signup-login'>
+       <p>Don't have an account??</p>
+       <button className='signup-button' onClick={()=>navigate('/signup')}>signup</button>
+    </div>
+    </div>
+    </div>
   );
 };
 
